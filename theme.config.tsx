bd21f2e6d68f7eb3cfde0cb2a9645a093ffc5308
Automation Expert <70540068+automationxpert@users.noreply.github.com>
2024-12-18 20:10:15 +0530

    update details for homepage

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,14 +4,14 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>KKE Docs</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/automationxpert/nextra-docs-template',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/automationxpert/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'KKE Devops-Labs Documentation',
   },
 }
 
